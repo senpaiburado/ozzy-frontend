8d1e5f5c4f3fd71357516403fa8f239b70152d4e
senpaiburado <minerminer354@gmail.com>
2021-06-15 03:51:48 +0300

    Excel

--- src/AdminTable.js
+++ src/AdminTable.js
@@ -1,11 +1,166 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import _ from 'lodash';
 import Datasheet from 'react-datasheet';
 import moment from "moment";
-import { BottomNavigation, BottomNavigationAction, Button, } from "@material-ui/core";
-import { ListAltOutlined, HistoryOutlined } from "@material-ui/icons";
+import { BottomNavigation, BottomNavigationAction, Button, Paper, Select, InputLabel, FormControl, MenuItem,
+        List, ListItem, Checkbox, ListItemIcon, ListItemText, Modal } from "@material-ui/core";
+import { ListAltOutlined, HistoryOutlined, PersonOutlineSharp } from "@material-ui/icons";
 import Alert from '@material-ui/lab/Alert';
+import ExcelJS from "exceljs/dist/es5/exceljs.browser";
+import saveAs from "file-saver";
+
+function createExcel(data) {
+    console.log(data)
+
+    var ExcelJSWorkbook = new ExcelJS.Workbook();
+    var worksheet = ExcelJSWorkbook.addWorksheet("ExcelJS sheet");
+
+    worksheet.mergeCells("A1:D1");
+    worksheet.mergeCells("A2:D2");
+
+    const timeCell = worksheet.getCell("A1");
+    timeCell.font = {
+      size: 20,
+    };
+    timeCell.alignment = { vertical: 'middle', horizontal: 'center' }
+    timeCell.value = moment().format("DD.MM.YYYY");
+
+    const titleCell = worksheet.getCell("A2");
+    titleCell.font = {
+      size: 20,
+      bold: true
+    };
+    titleCell.alignment = { vertical: 'middle', horizontal: 'center' }
+    titleCell.value = data[0].providerName;
+
+    worksheet.getCell("A3").value = "Назва закладу"
+    worksheet.getCell("B3").value = "Назва товару"
+    worksheet.getCell("C3").value = "К-сть"
+    worksheet.getCell("D3").value = "Од. виміру"
+
+    let index = 4;
+
+    data.forEach(user => {
+        const userCell = worksheet.getCell("A" + String(index));
+        userCell.value = user.name;
+        index++;
+
+        user.products.forEach(product => {
+            worksheet.getCell("B" + String(index)).value = product.product
+            worksheet.getCell("C" + String(index)).value = ( product.cu && product.cu.split(' ').length > 0 ? product.cu.split(' ')[0] : "" )
+            worksheet.getCell("D" + String(index)).value = ( product.cu && product.cu.split(' ').length > 0 ? product.cu.split(' ')[1] : "" )
+            index++;
+        })
+    })
+
+    ExcelJSWorkbook.xlsx.writeBuffer().then(function(buffer) {
+        saveAs(
+          new Blob([buffer], { type: "application/octet-stream" }),
+          `report.xlsx`
+        );
+      });
+}
+
+function ExportModal(props) {
+
+    const [selectedProviderId, setSelectedProviderId] = useState(1);
+
+    const [checked, setChecked] = React.useState([0]);
+
+    const handleToggle = (value) => () => {
+        const currentIndex = checked.indexOf(value);
+        const newChecked = [...checked];
+
+        if (currentIndex === -1) {
+        newChecked.push(value);
+        } else {
+        newChecked.splice(currentIndex, 1);
+        }
+
+        setChecked(newChecked);
+    };
+
+    let providers = [];
+    if (props.data && props.data.length) {
+        props.data.forEach(item => {
+            if (!providers.filter(x => x.id == item.providerId).length)
+            {
+                providers.push({ name: item.provider, id: item.providerId })
+            }
+        })
+    }
+
+    console.log(providers)
+
+    let users = [];
+    console.log(props.data)
+    if (props.data && props.data.length) {
+        props.data.forEach(item => {
+            if (!users.filter(x => x.id == item.userId && x.providerId == item.providerId).length)
+            {
+                users.push({
+                    id: item.userId,
+                    name: item.userName,
+                    products: props.data.filter(x => x.userId == item.userId && x.providerId == item.providerId).map(y => ({ product: y.product, cu: y.countAndUnit})),
+                    providerId: item.providerId,
+                    providerName: item.provider
+                })
+            }
+        })
+        console.log(users)
+    }
+
+    return (
+        <Paper>
+            <FormControl variant="outlined">
+            {/* <InputLabel id="demo-simple-select-outlined-label">Age</InputLabel> */}
+                <Select
+                labelId="demo-simple-select-outlined-label"
+                id="-1"
+                value={selectedProviderId}
+                onChange={(item) => { console.log(item); setSelectedProviderId(item.target.value) }}
+                label="Постачальник"
+                >
+                    <MenuItem value={-1}>
+                        <em>Не обрано</em>
+                    </MenuItem>
+                    { providers.map(item => {
+                        return <MenuItem value={item.id}>{item.name}</MenuItem>
+                    }) }
+                </Select>
+            </FormControl>
+
+            { users.filter(x => x.providerId == selectedProviderId).length > 0 ? (
+            <div>
+                <List>
+                {users.filter(x => x.providerId == selectedProviderId).map((value) => {
+                    const labelId = `checkbox-list-label-${value.id}`;
+
+                    return (
+                    <ListItem key={value} role={undefined} dense button onClick={handleToggle(value.id)}>
+                        <ListItemIcon>
+                        <Checkbox
+                            edge="start"
+                            checked={checked.indexOf(value.id) !== -1}
+                            tabIndex={-1}
+                            disableRipple
+                            inputProps={{ 'aria-labelledby': labelId }}
+                        />
+                        </ListItemIcon>
+                        <ListItemText id={labelId} primary={`${value.name}`} />
+                    </ListItem>
+                    );
+                })}
+                </List>
+
+                { users.filter(x => checked.includes(x.id) && x.providerId == selectedProviderId).length ? (
+                    <Button onClick={() => { createExcel(users.filter(x => checked.includes(x.id) && x.providerId == selectedProviderId)) }}>Сформувати</Button>
+                ) : <></>}
+            </div>) : <></> }
+        </Paper>
+    )
+}
 
 export default class BasicSheet extends React.Component {
     constructor(props) {
@@ -16,8 +171,15 @@ export default class BasicSheet extends React.Component {
             historyData: [],
             currentIndex: 0,
             pageIndex: 0,
-            isFetching: false
+            isFetching: false,
+            providers: [],
+            users: [],
+            isModalOpen: false
         };
+    }
+
+    handleModal(value) {
+        this.setState({ isModalOpen: value })
     }
 
     async componentDidMount() {
@@ -87,6 +249,12 @@ export default class BasicSheet extends React.Component {
             },
         });
 
+        const providersAnswer = await axios.get('http://23.100.51.147:1337/providers', {
+            headers: {
+                Authorization: "Bearer " + token.jwt
+            },
+        });
+
         console.log(usersAnswer.data);
         console.log(productsAnswer.data);
 
@@ -106,6 +274,7 @@ export default class BasicSheet extends React.Component {
                                 countAndUnit: product.count + " " + product.unit,
                                 userName: user.username,
                                 userAdress: user.address,
+                                userId: user.id,
                                 date: moment(order.date).format("DD-MM-YYYY"),
                                 provider: productObj && productObj.provider ? productObj.provider.Name : "",
                                 providerId: productObj && productObj.provider ? productObj.provider.id : -5,
@@ -176,6 +345,15 @@ export default class BasicSheet extends React.Component {
     render() {
         return (
             <div style={styles.root}>
+                <Modal
+                style={{ top: "45%", left: "40%", width: 450, minHeight: 150 }}
+                open={this.state.isModalOpen}
+                onClose={() => { this.handleModal(false) }}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+            >
+                <ExportModal data={this.state.data} />
+            </Modal>
                 { this.state.isFetching ? (<Alert severity="info">Зачейкайте, будь ласка! Завантаження...</Alert>) :
                     (
                         <div>
@@ -211,7 +389,10 @@ export default class BasicSheet extends React.Component {
                             :
                             (<div>
                                 <div style={styles.row}>
-                                    <Button onClick={this.approveFinished} variant="contained" color="primary">Підтвердити</Button>
+                                    <div style={styles.row}>
+                                    <Button style={{marginRight: 10}} onClick={this.approveFinished} variant="contained" color="primary">Підтвердити</Button>
+                                    <Button onClick={() => { this.handleModal(true) }} variant="contained" color="primary">Сформувати звіт</Button>
+                                    </div>
                                     <BottomNavigation
                                         value={this.state.pageIndex}
                                         onChange={(event, newValue) => {
